0cf3c8e33457b1e1a48e69c204226db03036dd0b
Saurav-pro <tiwarisaurav29@gmail.com>
2024-03-13 12:16:02 +0530

    Add project files.

---
+++ JWTTokenWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using JWTTokenWebApi.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTTokenWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string AddUser(Users users)
+        {
+            return "User Added with username: " + users.Username;
+        }
+    }
+}
---
+++ JWTTokenWebApi/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using JWTTokenWebApi.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWTTokenWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        private Users AuthenticateUser(Users user)
+        {
+            Users _users = null;
+            if (user.Username == "admin" && user.Password == "12345")
+            {
+                _users = new Users() { Username = "Saurav2901" };
+            }
+            return _users;
+        }
+
+        private string GenerateToken(Users users)
+        {
+            var SecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication"));
+            //var SecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var Credentials = new SigningCredentials(SecurityKey,SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"], null,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: Credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users users)
+        {
+            IActionResult response = Unauthorized();
+            var User_ = AuthenticateUser(users);
+            if (User_ != null) 
+            {
+                var token = GenerateToken(users);
+                response = Ok(new { token = token } );
+
+            }
+            return response;
+        }
+    }
+}
